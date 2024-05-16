99c820681b78270979745cf011018ad7e8031ce0
chopsticks-user <frostyfrost273@gmail.com>
2024-05-16 12:43:58 -0400

    url prefix

--- backend/rest_go/common/common.go
+++ backend/rest_go/common/common.go
@@ -4,6 +4,12 @@ import (
 	"strings"
 )
 
+var apiURLPrefix = "/api/v1"
+
 func IsBlank(s string) bool {
 	return s == "" || strings.TrimSpace(s) == ""
+}
+
+func URLWithPrefix(s string) string {
+	return apiURLPrefix + s
 }
--- backend/rest_go/main.go
+++ backend/rest_go/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sis/backend/api/common"
 	"sis/backend/api/routes"
 )
 
 func main() {
-	http.HandleFunc("/user/login", routes.UserLoginHandler)
-	http.HandleFunc("/course/", routes.CoursesHandler)
+	http.HandleFunc(common.URLWithPrefix("/user/login"), routes.UserLoginHandler)
+	http.HandleFunc(common.URLWithPrefix("/course/"), routes.CoursesHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error encountered while starting Go API at port 8080:", err)
